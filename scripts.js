c0bc11920678b02fbfedbdd71ad47a5a1406eed6
Paul Wu <paulwujr@gmail.com>
2019-10-16 20:03:04 -0400

    added new DOM

--- scripts.js
+++ scripts.js
@@ -15,3 +15,9 @@ container.appendChild(content);
 
 
 //an <h3> with blue text that says “I’m a blue h3!”//
+
+
+
+//a <div> with a black border and pink background color with the following elements inside of it:
+//another <h1> that says “I’m in a div”//
+//a <p> that says “ME TOO!”//
